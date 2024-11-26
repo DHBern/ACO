a6abab7ae681c42c0ff2aa6f3813334fbc00f895
vvvyyynet <63237439+vvvyyynet@users.noreply.github.com>
2024-11-26 17:24:09 +0100

    feat: add core routes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,14 @@
 <div class="prose prose-invert">
-	<h1>Welcome to SvelteKit</h1>
+	<h1>ACO</h1>
+	<h2>Die Akten des Konzils von Ephesus 431</h2>
 	<p>
-		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+		Das Konzil von Ephesus wurde 431, von Kaiser Theodosius II einberufen. Die Geschehnisse rund um
+		das Konzil von Ephesus sind uns in zahlreichen Akten überliefert. Die Akten wurden in den Jahren
+		1923–29 von Eduard Schwartz in einer epochalen, bis heute maßgeblichen Ausgabe ediert. Er beließ
+		die altsprachlichen Texte dabei in der Anordnung, die durch die einzelnen Sammlungen vorgegeben
+		war. Ziel unseres Projektes ist es, die Akten des Konzils erstmalig vollständig in eine moderne
+		Sprache zu übersetzen, chronologisch zu ordnen und zu kommentieren. Das Projekt wird von der DFG
+		gefördert und ist als Langfristvorhaben auf einen Zeitraum von zehn Jahren angesetzt
+		(2016–2026).
 	</p>
 </div>
