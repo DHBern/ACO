7eeb5d8802a8c43a0b88dc52563235064f6e75ab
Sebi <sebastian@flick.solutions>
2024-11-26 11:41:42 +0100

    setup tailwind & skeleton

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,22 @@ import containerQueries from '@tailwindcss/container-queries';
 import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
 
+import { skeleton, contentPath } from '@skeletonlabs/skeleton/plugin';
+import * as themes from '@skeletonlabs/skeleton/themes';
+
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}', contentPath(import.meta.url, 'svelte')],
 
 	theme: {
 		extend: {}
 	},
 
-	plugins: [typography, containerQueries]
+	plugins: [
+		skeleton({
+			// NOTE: each theme included will be added to your CSS bundle
+			themes: [themes.cerberus, themes.rose]
+		}),
+		typography,
+		containerQueries
+	]
 } satisfies Config;
