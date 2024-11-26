76bcbb2644f2d814670bdae89ce2543bee600d00
vvvyyynet <63237439+vvvyyynet@users.noreply.github.com>
2024-11-26 17:10:52 +0100

    fix: issue with import.meta
    This fixes "SyntaxError: Cannot use 'import.meta' outside a module"

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,9 +5,11 @@ import type { Config } from 'tailwindcss';
 import { skeleton, contentPath } from '@skeletonlabs/skeleton/plugin';
 import * as themes from '@skeletonlabs/skeleton/themes';
 
-export default {
-	content: ['./src/**/*.{html,js,svelte,ts}', contentPath(import.meta.url, 'svelte')],
+// Ensure contentPath is correctly resolved with `import.meta.url`
+const content = ['./src/**/*.{html,js,svelte,ts}', contentPath(import.meta.url, 'svelte')];
 
+export default {
+	content,
 	theme: {
 		extend: {}
 	},
