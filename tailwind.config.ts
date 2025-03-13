d8bb61e32b2d40aaaf0b134cb0d32ef9e244924d
vvvyyynet <levyn.buerki@unibe.ch>
2025-03-13 19:04:57 +0100

    fix: remove skeleton form tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,6 @@ import containerQueries from '@tailwindcss/container-queries';
 import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
 
-import { skeleton, contentPath } from '@skeletonlabs/skeleton/plugin';
-import * as themes from '@skeletonlabs/skeleton/themes';
-
 // Ensure contentPath is correctly resolved with `import.meta.url`
 const content = ['./src/**/*.{html,js,svelte,ts}', contentPath(import.meta.url, 'svelte')];
 
@@ -15,10 +12,6 @@ export default {
 	},
 
 	plugins: [
-		skeleton({
-			// NOTE: each theme included will be added to your CSS bundle
-			themes: [themes.cerberus, themes.rose]
-		}),
 		typography,
 		containerQueries
 	]
