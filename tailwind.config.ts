beb9a6142357bcaebfe9432f59ac17ced6c61fa4
vvvyyynet <levyn.buerki@unibe.ch>
2025-03-13 19:21:48 +0100

    fix: remove contentPath

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,7 @@ import containerQueries from '@tailwindcss/container-queries';
 import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
 
-// Ensure contentPath is correctly resolved with `import.meta.url`
-const content = ['./src/**/*.{html,js,svelte,ts}', contentPath(import.meta.url, 'svelte')];
-
 export default {
-	content,
 	theme: {
 		extend: {}
 	},
