9a8850396110572f51e149188a47135e2b2b7534
vvvyyynet <levyn.buerki@unibe.ch>
2025-03-19 15:22:11 +0100

    feat: add basic toc

---
+++ src/routes/edition/Content.svelte
@@ -0,0 +1,29 @@
+<script>
+    import { goto } from '$app/navigation';
+    import { textData } from '$lib/data/get_data';
+    
+    const types = ["CV","CPal","CVer","CU"];
+    let docs = {};
+    docs.CV = textData.filter(({ type: t }) => t === "CV");
+    docs.CPal = textData.filter(({ type: t }) => t === "CPal");
+    docs.CVer = textData.filter(({ type: t }) => t === "CVer");
+    docs.CU = textData.filter(({ type: t }) => t === "CU");
+</script>
+
+<h1>docs</h1>
+<p>These are the Documents of the first volume</p>
+
+<div class="">
+{#each types as type}
+    <div class="mt-14">
+        <h2 class="text-2xl">{type}</h2>
+        {#each docs[type] as doc}
+        <button onclick={()=>{goto(`edition/1/${doc.slug}/${doc.units[0]}`)}}>
+            <p class="text-lg">
+                <span class="font-bold">{doc.num}:</span> {doc.title} <span class="ml-1 text-sky-500 font-bold">({doc.slug})</span>
+            </p>
+        </button>        
+        {/each}
+    </div>
+    {/each}
+</div>
