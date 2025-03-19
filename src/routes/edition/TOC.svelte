7a99178c7dd23c896dd3b9d179b17d89fcf58fa0
vvvyyynet <levyn.buerki@unibe.ch>
2025-03-19 16:15:55 +0100

    feat: restyle toc

--- src/routes/edition/TOC.svelte
+++ src/routes/edition/TOC.svelte
@@ -11,26 +11,29 @@
 </script>
 
 <div class="flex-row flex-wrap pb-30">
-	<h1 class="text-4xl">Erster Band</h1>
+	<h1 class="mb-2 text-4xl">Erster Band</h1>
 	{#each types as type}
-		<h2 class="mt-10 text-2xl">{type}</h2>
+		<h2 class="mt-10 mb-4 text-2xl font-bold">{type}</h2>
 		<ul>
 			{#each docs[type] as doc}
-				<li class="hover:regular mb-1 text-[var(--aco-secondary)] hover:text-[var(--aco-teal)]">
-					<button
+				<li
+					class="mb-2 font-semibold text-[var(--aco-teal-secondary)] hover:text-[var(--aco-orange)]"
+				>
+					<a
+						href="#"
 						onclick={() => {
 							goto(`edition/1/${doc.slug}/${doc.units[0]}`);
 						}}
 					>
 						<span class="text-lg italic">"{doc.title}"</span>
 
-						<span class="ml-2 text-sm">
+						<span class="ml-2 text-sm text-[var(--aco-orange)]">
 							(
-							<span class="text-[var(--aco-teal)]">{doc.slug}</span> |
+							<span class="text-[var(--aco-orange-secondary)]">{doc.slug}</span> |
 							<span class="">{doc.num}</span>
 							)
 						</span>
-					</button>
+					</a>
 				</li>
 			{/each}
 		</ul>
