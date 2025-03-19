c05c5370de2f263391f89c16751d5c0a1da4ab0a
vvvyyynet <levyn.buerki@unibe.ch>
2025-03-19 15:07:30 +0100

    chore: unify +page.js files

--- src/routes/edition/[vol]/[doc]/+page.js
+++ src/routes/edition/[vol]/[doc]/+page.js
@@ -3,12 +3,20 @@ import { textData } from '$lib/data/get_data';
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch, params }) {
 	// URL Parameters
+    const slug_vol = params.vol;
 	const slug_doc = params.doc;
+    
+    // Get corresponding data
+    const document = textData.find(({ slug: s }) => s === slug_doc);
+    const dochead = document?.content.head;
+    const doctitle = document?.content.title;
 	
     // Get first unit of current document for redirect
-    const firstUnit = textData.find(({ slug: s }) => s === slug_doc).units[0];
+    const firstUnit = document?.units[0];
 
 	return {
+        dochead,
+        doctitle,
 		firstUnit,
         slug_doc
 	};
