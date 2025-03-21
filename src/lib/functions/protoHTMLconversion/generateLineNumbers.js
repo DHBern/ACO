b10f9833f0f417349e8b7dfe2833dfab0501e171
flicksolutions <sebastian@flick.solutions>
2025-03-21 17:29:35 +0100

    add queryparams to linenumbers

--- src/lib/functions/protoHTMLconversion/generateLineNumbers.js
+++ src/lib/functions/protoHTMLconversion/generateLineNumbers.js
@@ -1,3 +1,5 @@
+import { page } from '$app/state';
+
 export function generateLineNumbers(text) {
 	// Match a-line, p and br tags
 	const matches = text.match(/(<a\ data-line='\d+'><\/a>)|(<\/?p[^>]*>)|(<br\s*\/?>)/g);
@@ -5,7 +7,7 @@ export function generateLineNumbers(text) {
 
 	// Insert line-number as text at every 5th line
 	const numbersText = linesText.replace(/<a data-line='(\d+)'><\/a>/g, (match, lineNumber) => {
-		return `<a class="line-number" href="?line=${lineNumber}" line="${lineNumber}">${!(lineNumber % 5) ? lineNumber : "<span class='lineNumBuffer'>__</span>"}</a>`;
+		return `<a class="line-number" href="?${page.url.searchParams.toString()}&line=${lineNumber}" line="${lineNumber}">${!(lineNumber % 5) ? lineNumber : "<span class='lineNumBuffer'>__</span>"}</a>`;
 	});
 
 	return numbersText;
