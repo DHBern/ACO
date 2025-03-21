6c4e556043b69c369038ff6e4bafe8d43d4c3ed8
vvvyyynet <levyn.buerki@unibe.ch>
2025-03-21 15:10:36 +0100

    fix: click on popup wont reset mark

--- src/routes/edition/[vol]/[doc]/[unit]/MultiMarkPopup.svelte
+++ src/routes/edition/[vol]/[doc]/[unit]/MultiMarkPopup.svelte
@@ -21,9 +21,9 @@
 	});
 </script>
 
-<!-- class="multimark-popup absolute z-10 rounded-md border-2 border-[#94ffcf] border-white bg-[#e0fff1] p-5" -->
+<!-- class="absolute z-10 rounded-md border-2 border-[#94ffcf] border-white bg-[#e0fff1] p-5" -->
 <div
-	class="absolute max-w-[700px] border-4 border-[var(--aco-gray-1)] bg-[var(--aco-gray-1)] transition-transform duration-500 dark:bg-[var(--aco-teal)]"
+	class="multimark-popup absolute max-w-[700px] border-4 border-[var(--aco-gray-1)] bg-[var(--aco-gray-1)] transition-transform duration-500 dark:bg-[var(--aco-teal)]"
 	bind:this={popUpElement}
 >
 	<div class="p-1 font-bold">Mehrere Kommentare</div>
