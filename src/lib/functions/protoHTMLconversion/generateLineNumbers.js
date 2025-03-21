0895f6df5d15a3a7c70b73c528cd7da9bd891494
vvvyyynet <levyn.buerki@unibe.ch>
2025-03-21 18:23:43 +0100

    fix: conserve query params on page-num click

--- src/lib/functions/protoHTMLconversion/generateLineNumbers.js
+++ src/lib/functions/protoHTMLconversion/generateLineNumbers.js
@@ -1,12 +1,12 @@
 import { page } from '$app/state';
 
 export function generateLineNumbers(text) {
-	let href = page.url.searchParams;
 	// Match a-line, p and br tags
 	const matches = text.match(/(<a data-line='\d+'><\/a>)|(<\/?p[^>]*>)|(<br\s*\/?>)/g);
 	const linesText = matches ? matches.join('') : '';
 
 	// Insert line-number as text at every 5th line
+	let href = page.url.searchParams;
 	const numbersText = linesText.replace(/<a data-line='(\d+)'><\/a>/g, (_match, lineNumber) => {
 		href.set('line', lineNumber);
 		return `<a class="line-number" href="?${page.url.searchParams.toString()}" data-line="${lineNumber}">${!(lineNumber % 5) ? lineNumber : "<span class='lineNumBuffer'>__</span>"}</a>`;
--- src/lib/functions/protoHTMLconversion/generatePageNumbers.js
+++ src/lib/functions/protoHTMLconversion/generatePageNumbers.js
@@ -1,11 +1,16 @@
+import { page } from '$app/state';
+
 export function generatePageNumbers(text) {
 	// Match a-page, p and br tags
 	const matches = text.match(/(<a\ data-page='\d+'><\/a>)|(<\/?p[^>]*>)|(<br\s*\/?>)/g);
 	const pageText = matches ? matches.join('') : '';
 
 	// Insert page-number as text and add class
+	let href = page.url.searchParams;
 	const numbersText = pageText.replace(/<a data-page='(\d+)'><\/a>/g, (match, pageNumber) => {
-		return `<a class="page-number" href="?page=${pageNumber}" page="${pageNumber}">Seite ${pageNumber}</a>`;
+		href.set('page', pageNumber);
+		return `<a class="page-number" href="?${page.url.searchParams.toString()}" page="${pageNumber}">Seite ${pageNumber}</a>`;
 	});
+
 	return numbersText;
 }
