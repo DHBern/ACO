2c9229bee1898326bc7fd11d64c9b685dbf30273
flicksolutions <sebastian@flick.solutions>
2025-03-21 14:54:08 +0100

    set buildtarget to ES13 to allow top-level await

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [
-		tailwindcss(),
-		sveltekit()
-	]
+	plugins: [tailwindcss(), sveltekit()],
+	build: {
+		target: 'es2022'
+	}
 });
