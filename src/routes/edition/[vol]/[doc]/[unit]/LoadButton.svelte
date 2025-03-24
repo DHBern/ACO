488d3044eb0c3108bf3c91692a2c2ef352b9177f
vvvyyynet <levyn.buerki@unibe.ch>
2025-03-24 10:40:46 +0100

    fix: reposition LoadButtons

--- src/routes/edition/[vol]/[doc]/[unit]/LoadButton.svelte
+++ src/routes/edition/[vol]/[doc]/[unit]/LoadButton.svelte
@@ -3,9 +3,9 @@
 </script>
 
 {#if type == 'prev'}
-    <div class={`containerLoadButtonsTop ${classes}`}>
+    <div class={`flex justify-start containerLoadButtonsTop ${classes}`}>
 		<button
-			class="shadow-4xl m-3 mx-auto block min-w-[50px] bg-red-300 px-5 py-1"
+			class="shadow-4xl m-3 mx-110 block min-w-[50px] bg-red-300 px-5 py-1"
 			type="button"
 			onclick={(ev) => {
 				const firstUnit = groupedUnits[0];
@@ -34,10 +34,10 @@
 		>
 	</div>
 {:else if type == 'next'}
-	<div class={`containerLoadButtonsBottom ${classes}`}>
+	<div class={`flex justify-start containerLoadButtonsBottom ${classes}`}>
 		{#if data.docMetadata.slugs.findIndex((unit) => unit === groupedUnits[groupedUnits.length - 1].nextSlug) + 1}
 			<button
-				class="shadow-4xl m-3 mx-auto block min-w-[50px] bg-red-300 px-5 py-1"
+				class="shadow-4xl m-3 mx-110 block min-w-[50px] bg-red-300 px-5 py-1"
 				type="button"
 				onclick={(ev) => {
 					const lastUnit = groupedUnits[groupedUnits.length - 1];
