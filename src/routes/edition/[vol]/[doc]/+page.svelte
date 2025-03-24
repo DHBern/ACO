970e4f0ac9e3aed90e005d0289f530f54239e99f
vvvyyynet <levyn.buerki@unibe.ch>
2025-03-24 10:54:29 +0100

    testing: replace goto() with replaceState() (not working yet)

--- src/routes/edition/[vol]/[doc]/+page.svelte
+++ src/routes/edition/[vol]/[doc]/+page.svelte
@@ -1,10 +1,13 @@
 <script>
 	import { goto } from '$app/navigation';
 	import { onMount } from 'svelte';
+	import { replaceState } from '$app/navigation';
 
 	let { data } = $props();
 
 	onMount(() => {
 		goto(`./${data.slug_doc}/${data.docMetadata.slugs?.[0]}`);
+		// replaceState(`./${data.slug_doc}/${data.docMetadata.slugs?.[0]}`, ()=>{});
+		
 	});
 </script>
