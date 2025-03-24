ce67332214a45ec178080951e2c56a193b739e31
vvvyyynet <levyn.buerki@unibe.ch>
2025-03-24 10:02:32 +0100

    feat: add noteReferenceString superscripts for repeating words

--- src/lib/functions/protoHTMLconversion/createNoteReferenceString.js
+++ src/lib/functions/protoHTMLconversion/createNoteReferenceString.js
@@ -1,7 +1,17 @@
 export function createNoteReferenceString(lineStart, lineEnd, textStart, textEnd) {
+	// Superscripts as references for multiple words on one line
+	function convertSubToSup(text) {
+		let parts = text.split('_');
+		return parts.length > 1 ? parts[0] + '<sup>' + parts[1] + '</sup>' : text;
+	}
+	textStart = convertSubToSup(textStart);
+	textEnd = convertSubToSup(textEnd);
+
 	if (lineStart == lineEnd) {
+		// Print LIne number only once if identical
 		return `<span data-type='noteRefString-lines'>${lineStart}</span> <span data-type='noteRefString-text'>${textStart}&nbsp;&hellip;&nbsp;${textEnd}]</ span>`;
 	} else {
-        return `<span data-type='noteRefString-lines'>${lineStart}&nbsp;&ndash;&nbsp;${lineEnd}</span> <span data-type='noteRefString-text'>${textStart}&nbsp;&hellip;&nbsp;${textEnd}]</ span>`;
+		// Print both line numbers
+		return `<span data-type='noteRefString-lines'>${lineStart}&nbsp;&ndash;&nbsp;${lineEnd}</span> <span data-type='noteRefString-text'>${textStart}&nbsp;&hellip;&nbsp;${textEnd}]</ span>`;
 	}
 }
