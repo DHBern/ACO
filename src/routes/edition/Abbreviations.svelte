52fbbae1e07d88056bdfa2656c3251fc104cf5ae
vvvyyynet <levyn.buerki@unibe.ch>
2025-09-23 15:34:32 +0200

    feat: move abbreviations from accordion to drawer and add to main nav bar

--- src/routes/edition/Abbreviations.svelte
+++ src/routes/edition/Abbreviations.svelte
@@ -1,30 +1,42 @@
 <script>
 	import { abbData } from '$lib/data/get_data';
-    import { Accordion } from '@skeletonlabs/skeleton-svelte';
-    let accordionStateAbb = $state(['abb']);
-    let abbs = abbData;
+	import { Modal } from '@skeletonlabs/skeleton-svelte';
+	let drawerState = $state(false);
 
+	function drawerClose() {
+		drawerState = false;
+	}
+	let abbs = abbData;
 </script>
 
-<div class="flex-row flex-wrap bg-[var(--aco-gray-2)] dark:bg-[var(--aco-gray-3)]">
-	<Accordion
-		{accordionStateAbb}
-		onValueChange={(e) => (accordionStateAbb = e.value)}
-		collapsible
-		classes="containerAbbreviations shadow-md mb-4"
-	>
-		<Accordion.Item value="abb" classes="">
-			{#snippet lead()}{/snippet}
-			{#snippet control()}<span class="text-xl font-bold text-black dark:text-black">Abkürzungsverzeichnis</span
-				>{/snippet}
-			{#snippet panel()}
-			<div class="grid grid-cols-[150px_auto] gap-4 pl-13 pb-5">
+<Modal
+	open={drawerState}
+	onOpenChange={(e) => (drawerState = e.value)}
+	triggerBase="btn preset-filled"
+	contentBase="bg-surface-100-900 p-4 space-y-4 shadow-xl w-[480px] h-screen overflow-y-auto"
+	positionerJustify="justify-start"
+	positionerAlign=""
+	positionerPadding=""
+	transitionsPositionerIn={{ x: -480, duration: 200 }}
+	transitionsPositionerOut={{ x: -480, duration: 200 }}
+	classes="containerAbbreviations"
+>
+	{#snippet trigger()}Abkürzungsverzeichnis{/snippet}
+
+	{#snippet content()}
+		<header class="flex justify-between">
+			<h2 class="h2">Abkürzungsverzeichnis</h2>
+		</header>
+		<article>
+			<div class="grid grid-cols-[150px_auto] gap-4 pb-5 pl-13">
 				{#each Object.keys(abbs) as key}
 					<span class="col-span-1 col-start-1 font-bold">{key}</span>
 					<span class="col-span-1 col-start-2">{abbs[key]}</span>
 				{/each}
 			</div>
-			{/snippet}
-	</Accordion.Item>
-	</Accordion>
-</div>
+		</article>
+		<footer>
+			<button type="button" class="btn preset-filled" onclick={drawerClose}>Schliessen</button>
+		</footer>
+	{/snippet}
+</Modal>
