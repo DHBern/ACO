1620775667a76d5c20af892f87105582da8abeaa
vvvyyynet <levyn.buerki@unibe.ch>
2025-09-23 10:11:24 +0200

    style: remove buttons on end

--- src/routes/edition/[vol]/[doc]/[unit]/LoadButton.svelte
+++ src/routes/edition/[vol]/[doc]/[unit]/LoadButton.svelte
@@ -4,24 +4,7 @@
 
 {#if type == 'prev'}
 	<div class={`containerLoadButtonsTop flex justify-start ${classes}`} bind:this={node}>
-		<button
-			disabled={isDisabled}
-			class="shadow-4xl m-3 mx-110 block min-w-[50px] bg-red-300 px-5 py-1"
-			type="button"
-			onclick={() => {
-				clickHandler(myUnits);
-			}}
-		>
-			{#if isDisabled}
-				end reached
-			{:else}
-				<span class="font-bold">{myUnits[0].prevLabel}</span> laden
-			{/if}
-		</button>
-	</div>
-{:else if type == 'next'}
-	<div class={`containerLoadButtonsBottom flex justify-start ${classes}`} bind:this={node}>
-		{#if data.docMetadata.slugs.findIndex((unit) => unit === myUnits[myUnits.length - 1].nextSlug) + 1}
+		{#if !isDisabled}
 			<button
 				disabled={isDisabled}
 				class="shadow-4xl m-3 mx-110 block min-w-[50px] bg-red-300 px-5 py-1"
@@ -33,9 +16,30 @@
 				{#if isDisabled}
 					end reached
 				{:else}
-					<span class="font-bold">{myUnits[myUnits.length - 1].nextLabel}</span> laden
+					<span class="font-bold">{myUnits[0].prevLabel}</span> laden
 				{/if}
 			</button>
+		{/if}
+	</div>
+{:else if type == 'next'}
+	<div class={`containerLoadButtonsBottom flex justify-start ${classes}`} bind:this={node}>
+		{#if data.docMetadata.slugs.findIndex((unit) => unit === myUnits[myUnits.length - 1].nextSlug) + 1}
+			{#if !isDisabled}
+				<button
+					disabled={isDisabled}
+					class="shadow-4xl m-3 mx-110 block min-w-[50px] bg-red-300 px-5 py-1"
+					type="button"
+					onclick={() => {
+						clickHandler(myUnits);
+					}}
+				>
+					{#if isDisabled}
+						end reached
+					{:else}
+						<span class="font-bold">{myUnits[myUnits.length - 1].nextLabel}</span> laden
+					{/if}
+				</button>
+			{/if}
 		{/if}
 	</div>
 {/if}
