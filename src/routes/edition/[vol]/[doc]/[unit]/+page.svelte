30ce1e32eff1e60db92530cacb166cd0bba13a84
vvvyyynet <levyn.buerki@unibe.ch>
2025-09-30 14:21:27 +0200

    remove: no scroll without searchParams anymore

--- src/routes/edition/[vol]/[doc]/[unit]/+page.svelte
+++ src/routes/edition/[vol]/[doc]/[unit]/+page.svelte
@@ -240,11 +240,7 @@
 	});
 
 	function initialScroll() {
-		if (!(page.url.searchParams.get('line') || page.url.searchParams.get('page'))) {
-			// scroll window to document head
-			const elH1 = document.querySelector('.containerDocHead h1') as HTMLElement;
-			scrollStateInitWindow.scrollTo(scrollStateInitWindow.x, elH1?.offsetTop - 10 || 1);
-		} else {
+		if (page.url.searchParams.get('line') || page.url.searchParams.get('page')) {
 			// scroll window to document content
 			scrollStateInitWindow.scrollTo(
 				scrollStateInitWindow.x,
