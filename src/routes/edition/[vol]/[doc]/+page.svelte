6012b42d2948b562637bc0b77ccdb9e2f42bd96f
vvvyyynet <levyn.buerki@unibe.ch>
2025-10-15 13:49:45 +0200

    fix: revert fixing-commits and just add base-path

--- src/routes/edition/[vol]/[doc]/+page.svelte
+++ src/routes/edition/[vol]/[doc]/+page.svelte
@@ -2,11 +2,12 @@
 	import { goto } from '$app/navigation';
 	import { onMount } from 'svelte';
 	import { replaceState } from '$app/navigation';
+	import { base } from '$app/paths';
 
 	let { data } = $props();
 
 	onMount(() => {
-		goto(`./${data.slug_doc}/${data.docMetadata.slugs?.[0]}`);
+		goto(`${base}/edition/${data.slug_vol}/${data.slug_doc}/${data.docMetadata.slugs?.[0]}`);
 		// replaceState(`./${data.slug_doc}/${data.docMetadata.slugs?.[0]}`, ()=>{});
 	});
 </script>
