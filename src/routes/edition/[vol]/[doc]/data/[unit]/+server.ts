12390d38b9897cd95d80faebdb7bd75eb62d0620
vvvyyynet <levyn.buerki@unibe.ch>
2025-10-21 11:48:36 +0200

    fixing: generate entries for SSR

--- src/routes/edition/[vol]/[doc]/data/[unit]/+server.ts
+++ src/routes/edition/[vol]/[doc]/data/[unit]/+server.ts
@@ -1,15 +1,23 @@
+import type { EntryGenerator } from './$types';
+export const prerender = true;
+
 // Import local JSON files
+import { metaData as mData } from '$lib/data/aco-metadata.json';
 import { textData as tData } from '$lib/data/aco-text.json';
-import { abbData as aData } from '$lib/data/aco-abbreviations.json';
 import { notesData as nData } from '$lib/data/aco-notes.json';
-import { registerData as rData } from '$lib/data/aco-register.json';
 import { json } from '@sveltejs/kit';
 
+const metaData = await mData;
 const textData = await tData;
 const notesData = await nData;
-// const abbData = await aData;
-// const registerData = await rData;
-// export {textData, abbData, notesData, registerData};
+
+export const entries: EntryGenerator = () => {
+	return metaData.flatMap((m) => {
+		return m.unitSlugs.map((s) => {
+			return { vol: '1', doc: m.schwartzSlug, unit: s };
+		});
+	});
+};
 
 export async function GET({ params, fetch }) {
 	const unitText = textData?.[params.doc]?.[params.unit] || '';
