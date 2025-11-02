3dc13a8c7a01d4927db338e4b8c16dd9782f59eb
vvvyyynet <levyn.buerki@unibe.ch>
2025-11-02 13:22:52 +0100

    fix: adapt register to new data-format

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -3,22 +3,8 @@
 	import { Accordion } from '@skeletonlabs/skeleton-svelte';
 	let { data } = $props();
 
-	const placeLabels = data.places.map((el) => el.label);
-	const peopleLabels = data.people.map((el) => el.label);
-
 	let accordionPlaces = $state({ value: '' });
 	let accordionPeople = $state({ value: '' });
-
-	// Grouping the array by the first entry
-	function groupByDoc(loc) {
-		return loc.reduce((acc, [key, value]) => {
-			if (!acc[key]) {
-				acc[key] = [];
-			}
-			acc[key].push(value);
-			return acc;
-		}, {});
-	}
 
 	function convertUnit(unit) {
 		if (unit[0] === 'Z') {
@@ -27,18 +13,11 @@
 			return `/${unit}`;
 		}
 	}
-
-	const peopleGrouped = data.people.map((person) => {
-		return groupByDoc(person['loc-s']);
-	});
-	const placesGrouped = data.places.map((place) => {
-		return groupByDoc(place['loc-s']);
-	});
 </script>
 
-{#snippet accordion(labelsList, groupedList, accordionState)}
+{#snippet accordion(dataObject, accordionState)}
 	<Accordion {accordionState} onValueChange={(e) => (accordionState.value = e.value)} multiple>
-		{#each labelsList as label, idx}
+		{#each dataObject.map((el) => el.label) as label, idx}
 			<Accordion.Item value={label}>
 				<h3>
 					<Accordion.ItemTrigger class="hover:bg-primary-200-800"
@@ -48,10 +27,10 @@
 				<Accordion.ItemContent>
 					<div class="prose text-xl">
 						<ul>
-							{#each Object.keys(groupedList[idx]) as docKey}
+							{#each Object.keys(dataObject[idx]['loc-s']) as docKey}
 								<li>
 									<span data-type="register-doc-key">{docKey}</span>
-									{#each groupedList[idx][docKey] as unit}
+									{#each dataObject[idx]['loc-s'][docKey] as unit}
 										<a
 											href={`${base}/edition/1/${docKey}${convertUnit(unit)}`}
 											data-type="register-unit">{unit}</a
@@ -76,13 +55,13 @@
 	<div class="col-span-1 col-start-1">
 		<h2 class="h2 mb-4">Orte</h2>
 		<div class="bg-surface-100-900 h-[70vh] overflow-y-scroll p-3">
-			{@render accordion(placeLabels, placesGrouped, accordionPlaces)}
+			{@render accordion(data.places, accordionPlaces)}
 		</div>
 	</div>
 	<div class="col-span-1 col-start-2">
 		<h2 class="h2 mb-4">Personen</h2>
 		<div class="bg-surface-100-900 h-[70vh] overflow-y-scroll p-3">
-			{@render accordion(peopleLabels, peopleGrouped, accordionPeople)}
+			{@render accordion(data.people, accordionPeople)}
 		</div>
 	</div>
 </div>
