31f0413f8fbbdfbec2c2fd814b0fa9a04bd73989
vvvyyynet <levyn.buerki@unibe.ch>
2025-11-18 16:52:22 +0100

    fix: restyle literature

--- src/routes/edition/[vol]/literatur/+page.svelte
+++ src/routes/edition/[vol]/literatur/+page.svelte
@@ -13,10 +13,13 @@
 	:global(.rp-heading_1) {
 		@apply mt-10 text-5xl leading-tight font-semibold;
 	}
+	:global(section):not(:first-of-type) {
+		@apply mt-20;
+	}
 	:global(.rp-heading_2) {
-		@apply mt-10 text-4xl leading-tight font-semibold;
+		@apply text-4xl leading-tight font-semibold;
 	}
 	:global(li) {
-		@apply mt-4 text-2xl;
+		@apply mt-8 pl-6 -indent-6 text-2xl;
 	}
 </style>
