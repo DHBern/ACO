ce4fe96ba807582d44025fda79b46c7d81979478
vvvyyynet <levyn.buerki@unibe.ch>
2025-11-18 17:20:46 +0100

    feat: preface from json

--- src/routes/edition/[vol]/vorwort/+page.svelte
+++ src/routes/edition/[vol]/vorwort/+page.svelte
@@ -1,38 +1,7 @@
 <script>
+	import { preface } from '$lib/data/get_alldata_testing';
 </script>
 
 <div class="mx-auto max-w-[1500px] py-24">
-	<h1 class="h1">Vorwort</h1>
-	<p>
-		Das Projekt „Die Akten des Konzils von Ephesus 431. Übersetzung, Einleitung, Kommentar und
-		Register“, dem der hier vorliegende Band entsprungen ist, ist am Evangelisch-Theologischen
-		Seminar der Universität Bonn angesiedelt und steht unter Federführung von Prof. Dr. Wolfram
-		Kinzig, dem dortigen Inhaber des Lehrstuhls für Kirchengeschichte mit dem Schwerpunkt Alte
-		Kirchengeschichte. Es wird seit Januar 2016 als Langfristvorhaben von der DFG gefördert. Der
-		Projektgruppe gehören seit 2016 der Gräzist Dr. Thomas Brüggemann, der auch als Leiter der
-		Arbeitsstelle fungiert, und die Latinistin Dr. Sabine Lütkemeyer an. Sie sind auch für die
-		Übersetzung und Kommentierung der hier präsentierten Dokumente im Wesentlichen verantwortlich.
-		Seit dem Frühjahr 2022 werden sie bei ihrer Arbeit von der Theologin und Latinistin Dr. Julia
-		Beier unterstützt. Die Projektgruppe steht darüber hinaus in regelmäßigem Austausch mit einem
-		Kreis von Wissenschaftlerinnen und Wissenschaftlern, der in beratender Funktion hinzugerufen
-		werden kann. Diesem gehören namentlich an: die Latinistin Prof. Dr. Gerlinde Huber-Rebenich
-		(Bern), der Althistoriker Prof. Dr. Stefan Rebenich (Bern), der Gräzist Prof. Dr. Christoph
-		Riedweg (Zürich), der Patristiker Prof. Dr. Dr. h.c. mult. Adolf Martin Ritter (Heidelberg).
-	</p>
-	<p>
-		Die mit diesem Band und den folgenden Bänden veröffentlichten Arbeiten werden nicht nur in
-		gedruckter, sondern auch in digitaler Form erscheinen. Hierfür hat sich eine Zusammenarbeit mit
-		den Digital Humanities der Universität Bern entwickelt.
-	</p>
-	<p>
-		Wir danken zuvörderst der DFG und vor allem deren Gutachterinnen und Gutachtern für die
-		befürwortenden Stellungnahmen, die uns den Freiraum ermöglicht haben, das Projekt bis jetzt in
-		der vorgesehenen Weise zu realisieren. Darüber hinaus möchten wir auch den oben genannten
-		Gelehrten und allen anderen danken, die uns bei unserer Arbeit mit Rat und Tat zur Seite
-		gestanden haben. Hierunter zu nennen sind die Hilfskräfte, die für das Projekt tätig waren und
-		sind, die Teilnehmerinnen und Teilnehmer des von Herr Prof. Kinzig bzw. Frau Dr. Beier
-		angebotenen Lektürekurses, in dem mehrere der hier dargebotenen Texte besprochen werden konnten,
-		und natürlich alle Kolleginnen und Kollegen, die bereit waren, die sich aus unserer Arbeit
-		ergebenden Fragen mit uns zu diskutieren.
-	</p>
+	{@html preface}
 </div>
