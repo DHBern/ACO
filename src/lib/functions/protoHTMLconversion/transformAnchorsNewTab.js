c7975efc3a847ab265679f0d9181f65fe2e59ae7
vvvyyynet <levyn.buerki@unibe.ch>
2025-11-20 16:20:08 +0100

    fix: remove old asyncs in transformAnchorsNewTab

--- src/lib/functions/protoHTMLconversion/transformAnchorsNewTab.js
+++ src/lib/functions/protoHTMLconversion/transformAnchorsNewTab.js
@@ -1,14 +1,16 @@
+import { JSDOM } from 'jsdom';
+
 export function transformAnchorsNewTab(html) {
-	return import('jsdom').then(({ JSDOM }) => {
-		const dom = new JSDOM(`<div id="__ta_container__">${html}</div>`);
-		const doc = dom.window.document;
-		const container = doc.getElementById('__ta_container__');
+  // Wrap in a container to preserve fragment structure
+  const dom = new JSDOM(`<div id="__ta_container__">${html}</div>`);
+  const doc = dom.window.document;
+  const container = doc.getElementById('__ta_container__');
 
-		container.querySelectorAll('a').forEach((a) => {
-			if (!a.hasAttribute('target')) a.setAttribute('target', '_blank');
-			if (!a.hasAttribute('rel')) a.setAttribute('rel', 'noopener noreferrer');
-		});
+  // Update <a> elements
+  container.querySelectorAll('a').forEach(a => {
+    if (!a.hasAttribute('target')) a.setAttribute('target', '_blank');
+    if (!a.hasAttribute('rel')) a.setAttribute('rel', 'noopener noreferrer');
+  });
 
-		return container.innerHTML;
-	});
+  return container.innerHTML;
 }
--- src/lib/functions/protoHTMLconversion/transformFootnotesIntroduction.js
+++ src/lib/functions/protoHTMLconversion/transformFootnotesIntroduction.js
@@ -2,12 +2,12 @@ import { transformAnchorsNewTab } from './transformAnchorsNewTab';
 // Transform footnotes (sort by number and modify text)
 export function transformFootnotesIntroduction(object) {
 	return Object.keys(object)
-		.map(async (key) => {
+		.map((key) => {
 			// console.log(key)
 			const parts = key.split('-');
 			const num = parseInt(parts[1], 10);
 			
-			return { key, number: num, text: await transformAnchorsNewTab(object[key]) };
+			return { key, number: num, text: transformAnchorsNewTab(object[key]) };
 		})
 		.sort((a, b) => a.number - b.number);
 }
