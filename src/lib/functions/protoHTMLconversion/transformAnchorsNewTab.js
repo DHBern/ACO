acb2ddea9fd9f36a740197dff2b2554977e48fbe
vvvyyynet <levyn.buerki@unibe.ch>
2025-11-20 14:47:53 +0100

    testing testing testing

--- src/lib/functions/protoHTMLconversion/transformAnchorsNewTab.js
+++ src/lib/functions/protoHTMLconversion/transformAnchorsNewTab.js
@@ -1,12 +1,9 @@
-import { JSDOM } from 'jsdom';
-
-export function transformAnchorsNewTab(html) {
-  // Wrap in a container to preserve fragment structure
+export async function transformAnchorsNewTab(html) {
+  const { JSDOM } = await import('jsdom'); // dynamic import
   const dom = new JSDOM(`<div id="__ta_container__">${html}</div>`);
   const doc = dom.window.document;
   const container = doc.getElementById('__ta_container__');
 
-  // Update <a> elements
   container.querySelectorAll('a').forEach(a => {
     if (!a.hasAttribute('target')) a.setAttribute('target', '_blank');
     if (!a.hasAttribute('rel')) a.setAttribute('rel', 'noopener noreferrer');
