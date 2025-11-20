7df380e82052cea63b8240374acf949689f492b5
Sebi <sebastian@flick.solutions>
2025-11-20 15:06:05 +0100

    test synchronization

--- src/lib/functions/protoHTMLconversion/transformAnchorsNewTab.js
+++ src/lib/functions/protoHTMLconversion/transformAnchorsNewTab.js
@@ -1,13 +1,14 @@
-export async function transformAnchorsNewTab(html) {
-  const { JSDOM } = await import('jsdom'); // dynamic import
-  const dom = new JSDOM(`<div id="__ta_container__">${html}</div>`);
-  const doc = dom.window.document;
-  const container = doc.getElementById('__ta_container__');
+export function transformAnchorsNewTab(html) {
+	return import('jsdom').then(({ JSDOM }) => {
+		const dom = new JSDOM(`<div id="__ta_container__">${html}</div>`);
+		const doc = dom.window.document;
+		const container = doc.getElementById('__ta_container__');
 
-  container.querySelectorAll('a').forEach(a => {
-    if (!a.hasAttribute('target')) a.setAttribute('target', '_blank');
-    if (!a.hasAttribute('rel')) a.setAttribute('rel', 'noopener noreferrer');
-  });
+		container.querySelectorAll('a').forEach((a) => {
+			if (!a.hasAttribute('target')) a.setAttribute('target', '_blank');
+			if (!a.hasAttribute('rel')) a.setAttribute('rel', 'noopener noreferrer');
+		});
 
-  return container.innerHTML;
+		return container.innerHTML;
+	});
 }
