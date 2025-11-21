5b95492f76a3068c3ec55890e17a7100f6b7661a
vvvyyynet <levyn.buerki@unibe.ch>
2025-11-21 18:00:38 +0100

    fix: add base-path to links in introduction-footnotes

--- src/lib/functions/protoHTMLconversion/transformFootnotesIntroduction.js
+++ src/lib/functions/protoHTMLconversion/transformFootnotesIntroduction.js
@@ -1,4 +1,6 @@
 import { transformAnchorsNewTab } from './transformAnchorsNewTab';
+import { base } from '$app/paths';
+
 // Transform footnotes (sort by number and modify text)
 export function transformFootnotesIntroduction(object) {
 	return Object.keys(object)
@@ -6,8 +8,11 @@ export function transformFootnotesIntroduction(object) {
 			// console.log(key)
 			const parts = key.split('-');
 			const num = parseInt(parts[1], 10);
-			
-			return { key, number: num, text: transformAnchorsNewTab(object[key]) };
+			const text = transformAnchorsNewTab(object[key]).replace(
+				"href='/edition",
+				`href='${base}/edition`
+			);
+			return { key, number: num, text: text };
 		})
 		.sort((a, b) => a.number - b.number);
 }
