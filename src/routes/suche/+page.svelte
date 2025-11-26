9c320423f581f8564d80b43507658a3b2bb10b33
vvvyyynet <levyn.buerki@unibe.ch>
2025-11-26 17:22:48 +0100

    feat: add basic search functionality and some filters

--- src/routes/suche/+page.svelte
+++ src/routes/suche/+page.svelte
@@ -2,15 +2,20 @@
 	import { onMount } from 'svelte';
 	import { metaData } from '$lib/data/aco-metadata.json';
 
-	let q = $state('*');
-	let docFilter = $state('');
+	let query = $state('Lehre*');
+	let docFilter = $state('CPal22');
 	let start = $state(0);
 	let rows = 40;
 	let docs = $state([]);
 	let highlighting = $state({});
 	let numFound = $state(0);
 	let loading = $state(false);
-	let isChecked = $state(true);
+	let checkedSearchText = $state(true);
+	let checkedSearchNotes = $state(true);
+	let searchInTitle = $state(true);
+
+	let url = $state('');
+	$inspect(url);
 
 	const schwartzSlugs = metaData.map((m) => {
 		return m.schwartzSlug;
@@ -20,14 +25,24 @@
 		loading = true;
 		const params = new URLSearchParams();
 		// Trim input
-		// params.set('q', q.trim() || '*:*');
-		params.set('q', q.trim() || ':');
+		params.set('defType', 'edismax');
+		params.set('q', query.trim() || '*:*');
 
 		// Weights
-		// params.set('qf', 'title^5 docText^1 author^2');
+		if (searchInTitle) {
+			params.set('qf', 'title^1 aco_text_bare^1');
+		} else {
+			params.set('qf', 'aco_text_bare^1');
+		}
 		// params.set('pf', 'title^20');
 
 		// Filters
+		if (!checkedSearchText) {
+			params.append('fq', '-type:aco-unit');
+		}
+		if (!checkedSearchNotes) {
+			params.append('fq', '-type:aco-note');
+		}
 		if (docFilter) {
 			params.append('fq', `schwartzSlug:${docFilter}`);
 		}
@@ -37,25 +52,18 @@
 		params.set('rows', String(rows));
 
 		// Field List
-		// params.set('fl', 'id,type,author,title,entry,score');
-		params.set('fl', 'id,type,title,entry,score');
-		// params.set('fl', '*,score');
-
-		// https://hallernet-search.hidora.com/solr/public/select?q=*&start=0&rows=100&fq=author:[*%20TO%20*]&fl=title%2Cid%2Ctype%2Centry%2Cauthor%2Cscore&hl=true&wt=json
+		params.set('fl', '*,score');
 
 		// Highlighting
 		params.set('hl', 'true');
+		params.set('hl.snippets', '999999');
 		// params.set('hl.fl', 'title');
 
 		// Return json instead of xml
 		params.set('wt', 'json');
 
-		// loading = true;
-		// const params = new URLSearchParams();
-		// params.set('q', q.trim() || '*:*');
-		// if (docFilter) params.set('fq', `schwartzSlug:${docFilter}`);
+		url = `/api/solr?${params.toString()}`;
 
-		const url = `/api/solr?${params.toString()}`;
 		try {
 			const res = await fetch(url);
 			if (!res.ok) throw new Error(`Solr error ${res.status}`);
@@ -101,16 +109,22 @@
 	<h1>Suche</h1>
 
 	<form onsubmit={onSearchSubmit} class="search-form flex gap-10">
-		<input bind:value={q} placeholder="Search text..." />
-		<label>
-			<input type="checkbox" bind:checked={isChecked} /> Random Checkbox
-		</label>
+		<input bind:value={query} placeholder="Search text..." />
 		<select bind:value={docFilter}>
 			<option value="">All documents</option>
 			{#each schwartzSlugs as slug}
 				<option value={slug}>{slug}</option>
 			{/each}
 		</select>
+		<label>
+			<input type="checkbox" bind:checked={checkedSearchNotes} /> Search in notes
+		</label>
+		<label>
+			<input type="checkbox" bind:checked={checkedSearchText} /> Search in text
+		</label>
+		<label>
+			<input type="checkbox" bind:checked={searchInTitle} /> Include title
+		</label>
 		<button class="preset-filled rounded-full px-4 py-2" type="submit" disabled={loading}
 			>Search</button
 		>
@@ -119,36 +133,53 @@
 	{#if loading}
 		<p>Loading…</p>
 	{:else}
-		<p>{numFound} results</p>
-
+		<p>{numFound} Treffer</p>
 		<ul>
 			{#each docs as doc}
 				<li>
-					<div class="m-2 border-2 border-black p-3">
-						<div><strong>{doc.title}</strong> — <em>{doc.unit_slug}</em></div>
-						<div>
-							{#if highlighting[doc.id] && highlighting[doc.id].body}
-								{@html highlighting[doc.id].body.join(' ... ')}
-							{:else}
-								<p>{doc.first_line}</p>
-							{/if}
-						</div>
-						<small
-							>id: {doc.id ?? '-'} • type: {doc.type ?? '-'} • author: {doc.author
-								? doc.author[0]
-								: '-'} • score:
-							{Math.round((doc.score ?? 0) * 100) / 100}</small
+					<div
+						class={[
+							'm-2 border-2 border-5 border-black',
+							doc.type === 'aco-unit' && 'border-primary-50',
+							doc.type === 'aco-note' && 'border-secondary-50'
+						]}
+					>
+						<div
+							class={[
+								'px-3 py-1 text-xl',
+								doc.type === 'aco-unit' && 'bg-primary-50',
+								doc.type === 'aco-note' && 'bg-secondary-50'
+							]}
 						>
-						<!-- <p>{doc.docText ? doc.docText : 'no text'}</p> -->
-						<p>{doc.title ? doc.title[0] : 'no title'}</p>
+							<strong>{doc.schwartzSlug}: {doc.title}</strong>
+						</div>
+						<div class="p-3">
+							{#if highlighting[doc.id] && highlighting[doc.id].aco_text_bare}
+								{#each highlighting[doc.id].aco_text_bare as hl, idx}
+									<div class={['border-secondary-100 m-2 border-3 p-3']}>
+										<!-- <p>{@html Object.keys(highlighting[doc.id])}</p> -->
+										<!-- <p>{@html highlighting[doc.id].line}</p> -->
+										<p>{@html hl}</p>
+									</div>
+								{/each}
+							{:else}
+								<p>
+									{(doc.aco_text_bare || '').slice(0, 8) +
+										((doc.aco_text_bare || '').length > 8 ? '\n…' : '')}
+								</p>
+							{/if}
+							<!-- <p>
+								{doc.aco_text_bare ? doc.aco_text_bare : ''}
+							</p> -->
+						</div>
 					</div>
 				</li>
 			{/each}
 		</ul>
 
 		<div class="pagination">
-			<button onclick={prevPage} disabled={start === 0}>Previous</button>
-			<button onclick={nextPage} disabled={start + rows >= numFound}>Next</button>
+			<button onclick={prevPage} disabled={start === 0}>Zurück</button>
+			<button onclick={nextPage} disabled={start + rows >= numFound}>Weiter</button>
 		</div>
 	{/if}
 </div>
