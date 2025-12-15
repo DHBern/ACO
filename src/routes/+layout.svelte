7a236d6b03edc48c1251bcc8548123a923346f69
vvvyyynet <levyn.buerki@unibe.ch>
2025-12-15 17:49:17 +0100

    style: make menu responsive

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,26 +4,17 @@
 	import { goto } from '$app/navigation';
 	import { base } from '$app/paths';
 	import { page } from '$app/state';
-	import { AppBar, Dialog } from '@skeletonlabs/skeleton-svelte';
+	import { AppBar, Dialog, Portal } from '@skeletonlabs/skeleton-svelte';
 	import { Switch } from '@skeletonlabs/skeleton-svelte';
 	import Abbreviations from './edition/Abbreviations.svelte';
 	import { onMount } from 'svelte';
 	import IconMoon from '@lucide/svelte/icons/moon';
 	import IconSun from '@lucide/svelte/icons/sun';
+	import { Menu, X } from '@lucide/svelte';
 
 	let { children } = $props();
 	let openStateAbbreviations = $state(false);
 	let openStateMenu = $state(false);
-
-	let classesActive = $derived((href: string) =>
-		base + href === `/${page.url.pathname.split('/')[1]}`
-			? 'bg-primary-300 hover:bg-primary-300 hover:text-primary-700 text-primary-900'
-			: 'hover:text-primary-600'
-	);
-
-	function modalClose() {
-		openStateMenu = false;
-	}
 
 	// Lightswitch
 	let isDark = $state(false);
@@ -36,11 +27,12 @@
 		document.documentElement.setAttribute('data-darkModeState', darkModeState);
 	};
 
-	const pages = [
-		{ slug: 'Über das Projekt', path: '/ueber' },
+	const links = [
+		{ slug: 'Über&nbsp;das&nbsp;Projekt', path: '/ueber' },
 		{ slug: 'Bände', path: '/edition' },
 		{ slug: 'Register', path: '/register' },
 		{ slug: 'Suche', path: '/suche' },
+		{ slug: 'Literaturverzeichnis', path: '/edition/vol1/literatur' },
 		{ slug: 'Bibelstellen', path: '/bibelstellen' },
 		{ slug: 'Geovisualisierung', path: '/karte' },
 		{ slug: 'Bibliographie', path: '/bibliographie' }
@@ -57,116 +49,149 @@
 	});
 </script>
 
+<!-- Modal Abbrev -->
+<Abbreviations bind:openState={openStateAbbreviations} />
+
+<!-- Snippet Abbrev -->
+{#snippet abbreviations()}
+	<button
+		onclick={() => {
+			openStateAbbreviations = !openStateAbbreviations;
+			openStateMenu = false; // close menu
+		}}>Abkürzungsverzeichnis</button
+	>
+{/snippet}
+
+<!-- Snippet Lightswitch -->
+{#snippet lightswitch()}
+	<Switch
+		class="**:text-lx"
+		name="mode"
+		bind:checked={isDark.val}
+		onCheckedChange={handleToggleLightswitch}
+	>
+		<Switch.Control class="data-[state=checked]:bg-secondary-300 bg-surface-200">
+			<Switch.Thumb>
+				<Switch.Context>
+					{#snippet children(switch_)}
+						{#if switch_().checked}
+							<IconSun size="14" />
+						{:else}
+							<IconMoon size="14" />
+						{/if}
+					{/snippet}
+				</Switch.Context>
+			</Switch.Thumb>
+		</Switch.Control>
+		<Switch.HiddenInput />
+	</Switch>
+{/snippet}
+
 <div class="flex min-h-screen flex-col">
-	<!-- Modal Abbrev -->
-	<Abbreviations bind:openState={openStateAbbreviations} />
-
-	<!-- Snippet Abbrev -->
-	{#snippet abbreviations()}
-		<button
-			onclick={() => {
-				openStateAbbreviations = !openStateAbbreviations;
-			}}>Abkürzungsverzeichnis</button
-		>
-	{/snippet}
-
-	<!-- Snippet Lit -->
-	{#snippet literaturverzeichnis()}
-		<a href={`${base}/edition/1/literatur`}>Literaturverzeichnis</a>
-	{/snippet}
-
-	<!-- Snippet Lightswitch -->
-	{#snippet lightswitch()}
-		<Switch
-			class="**:text-lx"
-			name="mode"
-			bind:checked={isDark.val}
-			onCheckedChange={handleToggleLightswitch}
-		>
-			<Switch.Control class="data-[state=checked]:bg-secondary-300 bg-surface-200">
-				<Switch.Thumb>
-					<Switch.Context>
-						{#snippet children(switch_)}
-							{#if switch_().checked}
-								<IconSun size="14" />
-							{:else}
-								<IconMoon size="14" />
-							{/if}
-						{/snippet}
-					</Switch.Context>
-				</Switch.Thumb>
-			</Switch.Control>
-			<Switch.HiddenInput />
-		</Switch>
-	{/snippet}
-
 	<!-- Menu -->
-	<AppBar classes="px-4 py-0">
-		{#snippet lead()}
-			<!-- bg-primary-400-600 flex flex-none gap-10 text-xl text-slate-50 -->
-			<!-- ACO-Logo -->
-			<button
-				onclick={() => {
-					goto(base + '/.');
-				}}
-			>
-				<img src="{base}/logos/logo-aco.png" alt="ACO" class="max-h-full max-w-[100px]" />
+	<AppBar class="bg-primary-400-600 flex items-center justify-between px-2 py-0">
+		<!-- ACO-Logo / Home -->
+		<AppBar.Lead class="m-0 flex items-center p-0">
+			<button class="flex items-center py-2" onclick={() => goto(base + '/.')}>
+				<img src="{base}/logos/logo-aco.png" alt="ACO" class="mr-5 h-auto w-auto max-w-30" />
 			</button>
-		{/snippet}
+		</AppBar.Lead>
 
 		<!-- Top Navigation Bar -->
-		<nav>
-			<ul class="flex w-full items-center justify-around *:mr-10">
-				{#each pages as page}
-					<li class="list-nav-item inline-block h-full p-4 {classesActive(page.path)}">
-						<a href="{base}{page.path}">{page.slug}</a>
-					</li>
-				{/each}
-			</ul>
+		<AppBar.Headline class="m-0 mr-10">
+			<nav class="">
+				<ul
+					class="text-surface-50 my-8 hidden w-full items-start gap-x-8 gap-y-6 text-xl lg:flex lg:flex-wrap lg:justify-start"
+				>
+					{#each links as link}
+						<li
+							class={[
+								'list-nav-item hover:decoration-primary-300-700 inline-block h-full hover:underline hover:decoration-6',
+								base + link.path === `/${page.url.pathname.split('/')[1]}`
+									? 'decoration-secondary-300-700 underline decoration-6'
+									: ''
+							]}
+						>
+							<a href="{base}{link.path}">{@html link.slug}</a>
+						</li>
+					{/each}
 
-			<!-- Literautre -->
-			{@render literaturverzeichnis()}
+					<!-- Abbreviations -->
+					{@render abbreviations()}
+				</ul>
+			</nav>
+			<!-- Trail (Smartphone) -->
+		</AppBar.Headline>
 
-			<!-- Abbreviations -->
-			{@render abbreviations()}
+		<!-- Spacer -->
+		<div class="flex-grow"></div>
 
+		<!-- Menu for Smartphone -->
+		<AppBar.Trail class="mr-4">
 			<!-- Lightswitch -->
-			{@render lightswitch()}
-		</nav>
-		{#snippet trail()}
-			<Dialog
-				open={openStateMenu}
-				onOpenChange={(e) => (openStateMenu = e.open)}
-				positionerJustify="justify-start"
-				positionerAlign=""
-				positionerPadding="p-10"
-				transitionsPositionerIn={{ y: -480, duration: 200 }}
-				transitionsPositionerOut={{ y: -480, duration: 200 }}
-				backdropClasses="backdrop-blur-xl"
-			>
-				{#snippet trigger()}
-					<!-- this is an anchor tag because of node_invalid_placement warning -->
-					<!-- svelte-ignore a11y_missing_attribute -->
-					<!-- svelte-ignore a11y_click_events_have_key_events -->
-					<a tabindex="0" role="button" aria-label="Menü" class="btn-icon lg:!hidden">
-						<i class="fa-solid fa-bars"></i>
-					</a>
-				{/snippet}
-				{#snippet content()}
-					<nav class="list-nav">
-						<ul>
-							{#each pages as page}
-								<li>
-									<a href={`${base}${page.path}`} onclick={modalClose}>
-										<span class="flex-auto">{page.slug}</span>
-									</a>
-								</li>
-							{/each}
-						</ul>
-					</nav>
-				{/snippet}
-			</Dialog>
-		{/snippet}
+			<div class="hidden lg:block">{@render lightswitch()}</div>
+
+			<!-- Trigger aligned right on small screens -->
+			<div class="flex w-full justify-end lg:hidden">
+				<Dialog open={openStateMenu} onOpenChange={(e) => (openStateMenu = e.open)}>
+					<Dialog.Trigger
+						class="text-surface-50 ml-4 inline-flex items-center justify-center rounded p-2 hover:bg-white/10"
+					>
+						<Menu class="text-surface-50" width="40" height="40" />
+					</Dialog.Trigger>
+
+					<Portal>
+						<Dialog.Backdrop class="fixed inset-0 z-50 bg-black/50" />
+						<Dialog.Positioner class="fixed inset-0 z-50">
+							<Dialog.Content
+								class="bg-primary-400-600 text-surface-50 relative h-full w-full overflow-auto p-3"
+							>
+								<!-- Top bar with title and close in top-right -->
+								<div class="flex items-center justify-between">
+									<Dialog.Title class="text-lg font-semibold">Menu</Dialog.Title>
+
+									<div class="flex">
+										{@render lightswitch()}
+										<!-- Close button top-right -->
+										<Dialog.CloseTrigger
+											class="text-surface-50 ml-4 inline-flex items-center justify-center rounded p-2 hover:bg-white/10"
+											aria-label="Close menu"
+										>
+											<X width="40" height="40" />
+										</Dialog.CloseTrigger>
+									</div>
+								</div>
+
+								<!-- Nav content full height -->
+								<nav class="mt-6">
+									<ul class="flex flex-col gap-1">
+										{#each links as link}
+											<li class="m-2!">
+												<a
+													href={`${base}${link.path}`}
+													onclick={() => {
+														openStateMenu = false; // close Menu
+													}}
+													class="block px-2 py-1 text-lg"
+												>
+													{@html link.slug}
+												</a>
+											</li>
+										{/each}
+										<li class="m-2!">
+											<a class="block px-2 py-1 text-lg">{@render abbreviations()}</a>
+										</li>
+									</ul>
+
+									<!-- <div class="mt-6"> -->
+									<!-- </div> -->
+								</nav>
+							</Dialog.Content>
+						</Dialog.Positioner>
+					</Portal>
+				</Dialog>
+			</div>
+		</AppBar.Trail>
 	</AppBar>
 
 	<!-- Content -->
