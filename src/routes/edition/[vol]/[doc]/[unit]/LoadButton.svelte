94cf179aea0ebdd1233f22fc15f315c6cdf4be95
vvvyyynet <levyn.buerki@unibe.ch>
2025-12-15 19:19:01 +0100

    style: remove invalid shadow

--- src/routes/edition/[vol]/[doc]/[unit]/LoadButton.svelte
+++ src/routes/edition/[vol]/[doc]/[unit]/LoadButton.svelte
@@ -5,7 +5,7 @@
 {#if type == 'prev'}
 	<div class={`containerLoadButtonsTop flex justify-start ${classes}`} bind:this={node}>
 		<button
-			class="shadow-4xl bg-secondary-300-700 m-3 mx-110 block max-h-10 min-w-[200px] px-5 py-1"
+			class="bg-secondary-300-700 m-3 mx-110 block max-h-10 min-w-[200px] px-5 py-1"
 			type="button"
 			onclick={() => {
 				clickHandler(loadedUnits);
@@ -18,7 +18,7 @@
 	<div class={`containerLoadButtonsBottom flex justify-start ${classes}`} bind:this={node}>
 		{#if data.docMetadata.slugs.findIndex((unit) => unit === loadedUnits[loadedUnits.length - 1].nextSlug) + 1}
 			<button
-				class="shadow-4xl bg-secondary-300-700 m-3 mx-110 block max-h-10 min-w-[150px] px-5 py-1"
+				class="bg-secondary-300-700 m-3 mx-110 block max-h-10 min-w-[150px] px-5 py-1"
 				type="button"
 				onclick={() => {
 					clickHandler(loadedUnits);
