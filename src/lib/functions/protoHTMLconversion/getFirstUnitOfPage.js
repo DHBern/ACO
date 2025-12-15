1e3f738c02276b45ce06d38410dd76e6692b673b
vvvyyynet <levyn.buerki@unibe.ch>
2025-12-15 11:43:13 +0100

    chore: refactor to protoHTMLconversion function

---
+++ src/lib/functions/protoHTMLconversion/getFirstUnitOfPage.js
@@ -0,0 +1,32 @@
+export function getFirstUnitOfPage(pageParam, metaData) {
+	if (!pageParam) return { found: false};
+
+	const requested = parseInt(pageParam, 10);
+	if (Number.isNaN(requested)) return { found: false };
+
+	// iterate documents (assuming JSON is ordered according to documents)
+	for (const entry of metaData) {
+		// iterate unit keys in natural order of unitSlugs to respect ordering
+
+		for (const unitSlug of entry.unitSlugs) {
+			const limits = entry.pageLimits[unitSlug];
+			if (!limits) continue;
+			const lower = parseInt(limits[0], 10);
+			const upper = parseInt(limits[1], 10);
+			if (
+				!Number.isNaN(lower) &&
+				!Number.isNaN(upper) &&
+				requested >= lower &&
+				requested <= upper
+			) {
+				return {
+					found: true,
+					schwartzSlug: entry.schwartzSlug,
+					unitSlug,
+					page:requested
+				};
+			}
+		}
+	}
+	return { found: false, a:3 };
+}
--- src/routes/edition/[vol]/+layout.server.ts
+++ src/routes/edition/[vol]/+layout.server.ts
@@ -6,6 +6,7 @@ import type { LayoutServerLoad } from './$types';
 
 import { linkifyFootnoteSuperscripts } from '$lib/functions/protoHTMLconversion/linkifyFootnoteSuperscripts';
 import { transformFootnotesIntroduction } from '$lib/functions/protoHTMLconversion/transformFootnotesIntroduction';
+import { getFirstUnitOfPage } from '$lib/functions/protoHTMLconversion/getFirstUnitOfPage';
 
 const metaData = await mData;
 const metaDocs = await mDocs;
@@ -18,42 +19,8 @@ const preface = metaDocs.Vorwort;
 intro.text = linkifyFootnoteSuperscripts(intro.text);
 intro.footnotes = transformFootnotesIntroduction(intro.footnotes);
 
-// PageLimits
-function getPageLimits(pageParam) {
-	if (!pageParam) return { found: false};
-
-	const requested = parseInt(pageParam, 10);
-	if (Number.isNaN(requested)) return { found: false };
-
-	// iterate documents (assuming JSON is ordered according to documents)
-	for (const entry of metaData) {
-		// iterate unit keys in natural order of unitSlugs to respect ordering
-
-		for (const unitSlug of entry.unitSlugs) {
-			const limits = entry.pageLimits[unitSlug];
-			if (!limits) continue;
-			const lower = parseInt(limits[0], 10);
-			const upper = parseInt(limits[1], 10);
-			if (
-				!Number.isNaN(lower) &&
-				!Number.isNaN(upper) &&
-				requested >= lower &&
-				requested <= upper
-			) {
-				return {
-					found: true,
-					schwartzSlug: entry.schwartzSlug,
-					unitSlug,
-					page:requested
-				};
-			}
-		}
-	}
-	return { found: false, a:3 };
-}
-
 export const load: LayoutServerLoad = async ({ url, params }) => {
 	const pageParam = url.searchParams.get('page');
-	let pageInfo = getPageLimits(pageParam);
+	let pageInfo = getFirstUnitOfPage(pageParam, metaData);
 	return {vol : params.vol, pageInfo, intro, preface, abbData, litData };
 };
