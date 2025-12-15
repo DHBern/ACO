870afded2e52bbd2c010958556efda57be2a02c5
vvvyyynet <levyn.buerki@unibe.ch>
2025-12-15 12:08:06 +0100

    fix: stop applying protoHTMLconversion multiple times on footnotes

--- src/routes/edition/[vol]/+layout.server.ts
+++ src/routes/edition/[vol]/+layout.server.ts
@@ -3,18 +3,21 @@ export const prerender = true;
 import { metaDocs as mDocs } from '$lib/data/aco-meta-docs.json';
 import type { LayoutServerLoad } from './$types';
 
-import {linkifyFootnoteSuperscripts} from '$lib/functions/protoHTMLconversion/linkifyFootnoteSuperscripts'
-import {transformFootnotesIntroduction} from '$lib/functions/protoHTMLconversion/transformFootnotesIntroduction';
+import { linkifyFootnoteSuperscripts } from '$lib/functions/protoHTMLconversion/linkifyFootnoteSuperscripts';
+import { transformFootnotesIntroduction } from '$lib/functions/protoHTMLconversion/transformFootnotesIntroduction';
 
 const metaDocs = await mDocs;
 const litData = metaDocs.Literaturverzeichnis.text;
 const abbData = metaDocs.Abkürzungsverzeichnis.text;
-const intro = metaDocs.Einleitung;
-const preface = metaDocs.Vorwort;
+const introData = metaDocs.Einleitung;
+const prefData = metaDocs.Vorwort;
 
 // Modify protoHTML
-intro.text = linkifyFootnoteSuperscripts(intro.text);
-intro.footnotes = transformFootnotesIntroduction(intro.footnotes);
+const preface = prefData;
+const intro = {
+	text: linkifyFootnoteSuperscripts(introData.text),
+	footnotes: transformFootnotesIntroduction(introData.footnotes)
+};
 
 export const load: LayoutServerLoad = async () => {
 	return {
