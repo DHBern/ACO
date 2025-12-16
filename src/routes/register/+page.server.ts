e9abbeef553b55ef1e049ad8bbd8e79bc682ee3c
vvvyyynet <levyn.buerki@unibe.ch>
2025-12-16 16:50:42 +0100

    static: register -> server

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,7 +1,11 @@
+export const prerender = true;
+
 import { registerData } from '$lib/data/get_alldata_testing.js';
 
-/** @type {import('./$types').PageLoad} */
-export async function load({ fetch, params }) {
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ fetch, params }) => {
+
 	// Get corresponding data
 	const places = registerData.Orte;
 	const people = registerData.Personen;
