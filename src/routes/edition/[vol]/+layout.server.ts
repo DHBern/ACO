1750540bf9bbb6ef79469b070ea503faf7a54269
vvvyyynet <levyn.buerki@unibe.ch>
2025-12-19 13:40:52 +0100

    fix: move intro over to layout (sacrificing Einleitung but saving build)

--- src/routes/edition/[vol]/+layout.server.ts
+++ src/routes/edition/[vol]/+layout.server.ts
@@ -3,14 +3,21 @@ export const prerender = true;
 import { metaDocs } from '$lib/data/aco-meta-docs.json';
 import type { LayoutServerLoad } from './$types';
 
+import { linkifyFootnoteSuperscripts } from '$lib/functions/protoHTMLconversion/linkifyFootnoteSuperscripts';
+import { transformFootnotesIntroduction } from '$lib/functions/protoHTMLconversion/transformFootnotesIntroduction';
+
 const litData = metaDocs.Literaturverzeichnis.text;
 const abbData = metaDocs.Abkürzungsverzeichnis.text;
 const prefData = metaDocs.Vorwort;
+const introData = metaDocs.Einleitung;
 
 // Modify protoHTML
 const preface = prefData;
-
+const intro = {
+    text: linkifyFootnoteSuperscripts(introData.text),
+    footnotes: transformFootnotesIntroduction(introData.footnotes)
+};
 
 export const load: LayoutServerLoad = async () => {
-    return { preface, abbData, litData };
+    return { intro, preface, abbData, litData };
 };
--- src/routes/edition/[vol]/einleitung/+page.server.ts
+++ src/routes/edition/[vol]/einleitung/+page.server.ts
@@ -1,19 +1,19 @@
-export const prerender = true;
+// export const prerender = true;
 
-import { metaDocs } from '$lib/data/aco-meta-docs.json';
+// import { metaDocs } from '$lib/data/aco-meta-docs.json';
 
-import { linkifyFootnoteSuperscripts } from '$lib/functions/protoHTMLconversion/linkifyFootnoteSuperscripts';
-import { transformFootnotesIntroduction } from '$lib/functions/protoHTMLconversion/transformFootnotesIntroduction';
-import type { PageServerLoad } from './$types';
+// import { linkifyFootnoteSuperscripts } from '$lib/functions/protoHTMLconversion/linkifyFootnoteSuperscripts';
+// import { transformFootnotesIntroduction } from '$lib/functions/protoHTMLconversion/transformFootnotesIntroduction';
+// import type { PageServerLoad } from './$types';
 
-const introData = metaDocs.Einleitung;
+// const introData = metaDocs.Einleitung;
 
-// Modify protoHTML
-const intro = {
-    text: linkifyFootnoteSuperscripts(introData.text),
-    footnotes: transformFootnotesIntroduction(introData.footnotes)
-};
+// // Modify protoHTML
+// const intro = {
+//     text: linkifyFootnoteSuperscripts(introData.text),
+//     footnotes: transformFootnotesIntroduction(introData.footnotes)
+// };
 
-export const load: PageServerLoad = async () => {
-    return { intro };
-};
+// export const load: PageServerLoad = async () => {
+//     return { intro };
+// };
