86bc05f5b264c27d99f1b9e73aa7b83cfbc49c67
vvvyyynet <levyn.buerki@unibe.ch>
2026-01-18 15:33:07 +0100

    fix: preserve searchParams when forwarding to first unit on document-route

--- src/routes/dokumente/[vol]/[doc]/+page.svelte
+++ src/routes/dokumente/[vol]/[doc]/+page.svelte
@@ -1,13 +1,12 @@
 <script>
 	import { goto } from '$app/navigation';
 	import { onMount } from 'svelte';
-	import { replaceState } from '$app/navigation';
 	import { base } from '$app/paths';
+	import { page } from '$app/state';
 
 	let { data } = $props();
 
 	onMount(() => {
-		goto(`${base}/dokumente/${data.slug_vol}/${data.slug_doc}/${data.docMetadata.slugs?.[0]}`);
-		// replaceState(`./${data.slug_doc}/${data.docMetadata.slugs?.[0]}`, ()=>{});
+		goto(`${base}/dokumente/${data.slug_vol}/${data.slug_doc}/${data.docMetadata.slugs?.[0]}?${page.url.searchParams.toString()}`);
 	});
 </script>
