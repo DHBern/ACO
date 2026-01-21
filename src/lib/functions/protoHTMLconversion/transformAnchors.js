5a65bb7c728fba0fe1e874200818e3842d04cd1a
vvvyyynet <levyn.buerki@unibe.ch>
2026-01-21 16:17:40 +0100

    fix: prevent event propagation

--- src/lib/functions/protoHTMLconversion/transformAnchors.js
+++ src/lib/functions/protoHTMLconversion/transformAnchors.js
@@ -9,10 +9,16 @@ export function transformAnchors(html, refDocument = null) {
 
 	// Update <a> elements
 	container.querySelectorAll('a').forEach((a) => {
-		if (!refDocument || a.getAttribute('document') !== refDocument){
-			if (!a.hasAttribute('target')) a.setAttribute('target', '_blank');
+		// prevent event propagation
+		a.setAttribute('onclick', 'event.stopPropagation();');
+
+		// open documents to *other* documents in new tab
+		if (!refDocument || a.getAttribute('document') !== refDocument) {
 			if (!a.hasAttribute('rel')) a.setAttribute('rel', 'noopener noreferrer');
+			if (!a.hasAttribute('target')) a.setAttribute('target', '_blank');
 		}
+
+		// add base path to href
 		const orig = a.getAttribute('href');
 		if (orig && orig.startsWith('/')) {
 			a.setAttribute('href', base + orig);
