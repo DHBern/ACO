8f9cb965e1da783275a88b726c828f4094286266
vvvyyynet <levyn.buerki@unibe.ch>
2026-03-31 14:39:49 +0200

    test: add console logs for diagnostics

--- src/routes/dokumente/[vol]/+page.svelte
+++ src/routes/dokumente/[vol]/+page.svelte
@@ -8,7 +8,13 @@
 
 	onMount(() => {
 		// Redirect to first unit on requested page
+		console.log('HI');
+		console.log('pageparam',data.pageParam);
+		console.log('found',  data.pageParamRedirect.found);
 		if (data.pageParam && data.pageParamRedirect.found) {
+			console.log(data.pageParamRedirect.schwartzSlug);
+			console.log(data.pageParamRedirect.unitSlug);
+			console.log(data.pageParamRedirect.page);
 			goto(
 				resolve(`/dokumente/${data.vol}/${data.pageParamRedirect.schwartzSlug}/${data.pageParamRedirect.unitSlug}?page=${data.pageParamRedirect.page}`),
 				{ replaceState: true }
