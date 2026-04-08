83ff50432667b2e5a447485620387e15c4b95d45
vvvyyynet <levyn.buerki@unibe.ch>
2026-04-08 09:10:26 +0200

    fix: add entryGenerator for doc-routes (without unit)

---
+++ src/routes/dokumente/[vol]/[doc]/+page.ts
@@ -0,0 +1,14 @@
+export const prerender = false;
+import type { EntryGenerator } from './$types';
+
+import { metaData } from '$lib/data/aco-metadata.json';
+
+export const entries: EntryGenerator = () => {
+    return [
+        ...metaData.flatMap((m) => {
+            return m.unitSlugs.map((s) => {
+                return { vol: 'vol1', doc: m.schwartzSlug};
+            });
+        }),
+    ];
+};
